a1cd99ba5a0a81e9c9559653cc3bd58ec5491e4f
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2025-10-10 20:24:02 +0530

    List completed!!

--- lib/dart/ds/lists.dart
+++ lib/dart/ds/lists.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 void main(){
 
   // Deceleration of a static List.
   List<String> list = ["Aditya","Sandesh","Jivesh","Parth","Varad","Rohan"];
+
+  // Declaration of Dynamic List.
+  List<String> list1 = [];
 
   // Iteration:
   // for(int i=0;i<list.length;i++)
@@ -17,11 +22,41 @@ void main(){
   // .toList();
   // print(result);
 
+  // print("Enter the no of elements");
+  // int? no = int.tryParse(stdin.readLineSync()?? '');
+  //
+  // for(int i=0;i<no!;i++){
+  //   print("Enter the element");
+  //   list1.add((stdin.readLineSync()?? ''));
+  // }
+  //
+  // print("Enter the key");
+  // String? key = stdin.readLineSync();
+  //
+  // print(list1.where((value)=> value==key)
+  // .toList());
 
-  if(list.contains("Aditya"))
-    print("Present");
-  else
-    print("Absent");
+
+  // Functions:
+
+  /*
+   1) for appending element in the list: list.add(value)
+   2) for adding a list to another list: list1.addAll(list2)
+   3) for inserting element at a position: list.insert(index,object)
+   4) for inserting a list of element after a index: list2.insertAt(index,list2)
+   5) for Removing a value from list: list.remove(value)
+   6) for removing a value at a given index: list.removeAt(index)
+   7) for getting the length of the list: list.length()
+   8) for reversing the list: list.reverse()
+   9) whether list is empty or not: list.isEmpty or list.isNotEmpty
+
+   */
+
+
+  // For Each Loop:
+
+  for(var value in list)
+    stdout.write("$value ");
 
 
 
