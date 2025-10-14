3b3cf7fb5f47d3d71668188693c7715bdbf944cb
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2025-10-14 20:27:17 +0530

    Various Buttons covered

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart' hide Size;
+import 'package:flutter/material.dart' hide Size;
 
 // <--------------------------------------------------------StateLess Widget----------------------------------------------------------------------------->
 
@@ -100,7 +102,7 @@ class ImageWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Radient Dermat"),
+        title: Text("Image Widget"),
         backgroundColor: Colors.blue,
         centerTitle: true,
       ),
@@ -140,10 +142,164 @@ class CircularImage extends StatelessWidget {
           "https://fastly.picsum.photos/id/738/200/300.jpg?hmac=x-GpfBAQrKyKnrXqne7UJ3rdVnkGD7e-uRhpCcZWb9I",
         ),
         radius: 50,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.transparent,
       ),
     );
   }
 }
+
+// <--------------------------------------------------------------- Normal Button Widget----------------------------------------------------------------------------->
+
+class ButtonWidget extends StatefulWidget {
+  const ButtonWidget({super.key});
+
+  @override
+  State<ButtonWidget> createState() => _ButtonWidgetState();
+}
+
+class _ButtonWidgetState extends State<ButtonWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Button Widget"),
+        centerTitle: true,
+        backgroundColor: Colors.grey,
+      ),
+      body: ElevatedButton(
+        onPressed: () {
+          print("Data Submitted!!");
+        },
+        onLongPress: () {
+          print("Long Pressed");
+        },
+        child: Text("Submit"),
+        style: ElevatedButton.styleFrom(
+          // For Styling the Button
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          elevation: 10,
+          textStyle: TextStyle(
+            // For Styling the Text of Button
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        // For Styling Button
+      ),
+    );
+  }
+}
+
+// <---------------------------------------------------------------Icon Button Widget----------------------------------------------------------------------------->
+
+class IconButton extends StatefulWidget {
+  const IconButton({super.key});
+
+  @override
+  State<IconButton> createState() => _IconButtonState();
+}
+
+class _IconButtonState extends State<IconButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ElevatedButton.icon(
+        onPressed: () {
+          print("Login Successful");
+        },
+        icon: Icon(Icons.account_circle),
+        label: Text(
+          "Login",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.blueGrey,
+          elevation: 10,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
+          iconColor: Colors.black,
+          iconSize: 20,
+          iconAlignment: IconAlignment.start,
+        ),
+      ),
+    );
+  }
+}
+
+// <---------------------------------------------------------------Outline Button Widget----------------------------------------------------------------------------->
+
+class OutlineButtonWidget extends StatefulWidget {
+  const OutlineButtonWidget({super.key});
+
+  @override
+  State<OutlineButtonWidget> createState() => _OutlineButtonWidgetState();
+}
+
+class _OutlineButtonWidgetState extends State<OutlineButtonWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: OutlinedButton(
+        onPressed: () {
+          print("Outline Button");
+        },
+        child: Text("Submit"),
+        style: OutlinedButton.styleFrom(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          side: BorderSide(color: Colors.red, width: 5),
+          textStyle: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+            color: Colors.white,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// There is also Icon Button and Text Button
+
+// <----------------------------------------------------------------------Floating Action Button Widget----------------------------------------------------------------------------->
+
+// We have to place Floating Action Button not in child, we have to store it in floatingActionButton: Section
+
+class FloatActionButtonWidget extends StatefulWidget {
+  const FloatActionButtonWidget({super.key});
+
+  @override
+  State<FloatActionButtonWidget> createState() =>
+      _FloatActionButtonWidgetState();
+}
+
+class _FloatActionButtonWidgetState extends State<FloatActionButtonWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(""),
+      ),
+        floatingActionButton:FloatingActionButton(onPressed: (){
+        print("Floating Action Button");
+        },
+        child: Icon(Icons.add,
+        size: 30,
+          color: Colors.black,
+        ),
+          backgroundColor: Colors.greenAccent,
+        )
+    );
+  }
+}
+
+// <---------------------------------------------------------------My-App Widget----------------------------------------------------------------------------->
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -152,7 +308,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: CircularImage()),
+      home: Scaffold(
+        body: FloatActionButtonWidget(),
+      ),
     );
   }
 }
